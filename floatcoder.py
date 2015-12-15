9422198ce2c2d5f4739fb71c2417a8eb7e7042eb
Alex Stewart <Alexander.Stewart@consensuscorp.com>
2015-12-15 08:54:28 -0800

    Add initial DPR stuff (still kinda rough but functional)

--- floatcoder.py
+++ floatcoder.py
@@ -22,20 +22,24 @@ def count_zeros(value):
     return result
 
 def float_to_int(f):
-    i = struct.unpack('I', struct.pack('f', f))[0]
+    return struct.unpack('I', struct.pack('f', f))[0]
+
+def int_to_float(i):
+    return struct.unpack('f', struct.pack('I', i))[0]
+
+def flint_trans(i):
     if i & (1 << 31):
         i ^= 0xffffffff
     else:
         i ^= (1 << 31)
     return i
 
-def int_to_float(i):
+def flint_untrans(i):
     if i & (1 << 31):
         i ^= (1 << 31)
     else:
         i ^= 0xffffffff
-    f = struct.unpack('f', struct.pack('I', i))[0]
-    return f
+    return i
 
 class EOFError (Exception):
     pass
@@ -177,6 +181,97 @@ class Decoder:
 
 
 class Float_Encoder:
+    def __init__(self, outfile, predictor, coder=None):
+        self.predictor = predictor
+        if coder:
+            self.rc = coder
+        else:
+            model = qsmodel.QSModel(64, MODEL_BITS, 2000)
+            self.rc = Encoder(outfile, model)
+        self.count = 0
+
+    def encode_float(self, v):
+        i = float_to_int(v)
+        #FIXME: eventually need to convert to int prior to prediction step (for portability)
+        p = flint_trans(float_to_int(self.predictor.next()))
+        i = flint_trans(i)
+        d = (i - p) & 0xffffffff
+        log("E:{}: v={}, i={:08x}, p={:08x}, d={:08x}".format(self.count, v, i, p, d))
+        self.encode(d)
+        self.predictor.update(int_to_float(flint_untrans(i)))
+        self.count += 1
+
+    def encode(self, value):
+        if not (value & (1 << 31)):
+            # Positive value
+            zeros = count_zeros(value)
+            # top bit is zero, so count_zeros will always be between 1 and 32
+            sym = zeros - 1
+            # result: 0 <= sym <= 31
+        else:
+            # Negative value
+            value = (value & 0xffffffff) ^ 0xffffffff
+            zeros = count_zeros(value)
+            # top bit is also now zero, so count_zeros will always be between 1 and 32
+            sym = 32 | (zeros - 1)
+            # result: 32 <= sym <= 63
+        log("- sym=0x{:02x}".format(sym))
+        self.rc.encode_sym(sym)
+        # First bit after zeros is (by definition) always 1, so no need to send it.
+        bits = 32 - zeros - 1
+        if bits > 0:
+            range = 1 << bits
+            log("- value=0x{:x} ({})".format(value ^ range, bits))
+            self.rc.encode_value(value ^ range, bits)
+
+    def done(self):
+        self.rc.done()
+
+
+class Float_Decoder:
+    def __init__(self, infile, predictor, coder=None):
+        self.predictor = predictor
+        if coder:
+            self.rc = coder
+        else:
+            model = qsmodel.QSModel(64, MODEL_BITS, 2000, compress=False)
+            self.rc = Decoder(infile, model)
+        self.count = 0
+
+    def decode_float(self):
+        p = flint_trans(float_to_int(self.predictor.next()))
+        d = self.decode()
+        i = (p + d) & 0xffffffff
+        v = int_to_float(flint_untrans(i))
+        log("D:{}: v={}, i={:08x}, p={:08x}, d={:08x}".format(self.count, v, i, p, d))
+        self.predictor.update(v)
+        self.count += 1
+        return v
+
+    def decode(self):
+        sym = self.rc.decode_sym()
+        log("- sym=0x{:02x}".format(sym))
+        sign = sym >> 5
+        zeros = (sym & 0x1f) + 1
+        bits = 32 - zeros - 1
+        log("- zeros={} bits={}".format(zeros, bits))
+        if bits > 0:
+            range = 1 << bits
+            value = self.rc.decode_value(bits) | range
+        elif bits == 0:
+            value = 1
+        else:
+            value = 0
+        log("- value=0x{:x} ({})".format(value, bits))
+        if sign:
+            value ^= 0xffffffff
+        return value
+
+    def done(self):
+        self.rc.done()
+
+
+class Float_Encoder_SPR:
     def __init__(self, outfile, predictor, coder=None, spr=0):
         self.predictor = predictor
         if coder:
@@ -226,7 +321,7 @@ class Float_Encoder:
         self.rc.done()
 
 
-class Float_Decoder:
+class Float_Decoder_SPR:
     def __init__(self, infile, predictor, coder=None, spr=0):
         self.predictor = predictor
         if coder:
@@ -267,6 +362,204 @@ class Float_Decoder:
         if sign:
             value ^= self.spr_mask
         return value
+
+    def done(self):
+        self.rc.done()
+
+
+class Float_Encoder_DPR:
+    def __init__(self, outfile, predictor, coder=None, dpr=None):
+        self.predictor = predictor
+        if coder:
+            self.rc = coder
+        else:
+            model = qsmodel.QSModel(65, MODEL_BITS, 2000)
+            self.rc = Encoder(outfile, model)
+        self.dpr = dpr
+        self.count = 0
+
+    def encode_float(self, v):
+        i = float_to_int(v)
+        dpr_exp = ((i >> 23) & 0xff) - 0x7f
+        log("-- dpr_exp={}".format(dpr_exp))
+        if dpr_exp > 0x7f:
+            # Extranormal value (infinity, NaN).. don't mess with these.
+            dpr_bits = 23
+        else:
+            dpr_bits = dpr_exp - self.dpr
+            if dpr_bits < 0:
+                # Value is below our minimum threshold, just make it zero
+                dpr_bits = 0
+                i = 0
+            elif dpr_bits > 23:
+                # No rounding necessary
+                dpr_bits = 23
+        trim_bits = 23 - dpr_bits
+        trim_mask = (1 << 32) - (1 << trim_bits)
+
+        #FIXME: eventually need to convert to int prior to prediction step (for portability)
+        p = flint_trans(float_to_int(self.predictor.next())) & trim_mask
+        i = flint_trans(i) & trim_mask
+        p_exp = p & 0xff800000
+        p_mant = p & 0x007fffff
+        i_exp = i & 0xff800000
+        i_mant = i & 0x007fffff
+        if i > p:
+            sign = 0
+            d_exp = (i_exp - p_exp) & 0xff800000
+            d_mant = (i_mant - p_mant) & 0x007fffff
+        else:
+            sign = 1
+            d_exp = (p_exp - i_exp) & 0xff800000
+            d_mant = (p_mant - i_mant) & 0x007fffff
+        d = d_exp | d_mant
+        #d = (i - p) & 0xffffffff
+        log("E:{}: v={}, i={:08x}/{:08x}:{:08x}, p={:08x}/{:08x}:{:08x}, d={:08x}/{:08x}:{:08x}".format(self.count, v, i, i_exp, i_mant, p, p_exp, p_mant, d, d_exp, d_mant))
+        self.encode(sign, d, trim_bits)
+        pu = int_to_float(flint_untrans(i) & trim_mask)
+        log("! updating predictor with: {}".format(pu))
+        self.predictor.update(pu)
+        self.count += 1
+
+    def encode(self, sign, value, trim_bits):
+        # top bit is always zero, so count_zeros will always be between 1 and 32
+        zeros = count_zeros(value)
+        if value == 0:
+            sym = 64
+        elif not sign:
+            # Positive value
+            sym = zeros
+            # result: 0 <= sym <= 31
+        else:
+            # Negative value
+            sym = 32 | zeros
+            # result: 32 <= sym <= 63
+        log("- sym=0x{:02x}".format(sym))
+        self.rc.encode_sym(sym)
+        # First bit after zeros is (by definition) always 1, so no need to send it.
+        bits = 32 - zeros - 1
+        if bits > 23:
+            exp_bits = bits - 23
+            mant_bits = 23
+            range = 1 << bits
+            bitval = value ^ range
+        elif bits > 0:
+            exp_bits = 0
+            mant_bits = bits
+            range = 1 << bits
+            bitval = value ^ range
+        else:
+            exp_bits = 0
+            mant_bits = 0
+            bitval = 0
+
+        mant_bits = max(mant_bits - trim_bits, 0)
+
+        log("- zeros={} bits={} exp_bits={} mant_bits={} trim_bits={}".format(zeros, bits, exp_bits, mant_bits, trim_bits))
+        if exp_bits:
+            log("- encoding exp_bits: {:02x}/{}".format(bitval >> 23, exp_bits))
+            self.rc.encode_value(bitval >> 23, exp_bits)
+        if mant_bits:
+            mantval = (bitval & ((1 << 23) - 1)) >> trim_bits
+            log("- encoding mant_bits: {:x}/{}".format(mantval, mant_bits))
+            self.rc.encode_value(mantval, mant_bits)
+
+    def done(self):
+        self.rc.done()
+
+
+class Float_Decoder_DPR:
+    def __init__(self, infile, predictor, coder=None, dpr=None):
+        self.predictor = predictor
+        if coder:
+            self.rc = coder
+        else:
+            model = qsmodel.QSModel(65, MODEL_BITS, 2000, compress=False)
+            self.rc = Decoder(infile, model)
+        self.dpr = dpr
+        self.count = 0
+
+    def decode_float(self):
+        p = flint_trans(float_to_int(self.predictor.next()))
+        p_exp = p & 0xff800000
+        p_mant = p & 0x007fffff
+        sign, d, trim_bits = self.decode(p_exp)
+        trim_mask = (1 << 32) - (1 << trim_bits)
+        p_mant &= trim_mask
+        d_exp = d & 0xff800000
+        d_mant = d & 0x007fffff
+        if sign:
+            i_exp = (p_exp - d_exp) & 0xff800000
+            i_mant = (p_mant - d_mant) & 0x007fffff
+        else:
+            i_exp = (p_exp + d_exp) & 0xff800000
+            i_mant = (p_mant + d_mant) & 0x007fffff
+        i = i_exp | i_mant
+        #i = (p + d) & 0xffffffff
+        v = int_to_float(flint_untrans(i) & trim_mask)
+        log("D:{}: v={}, i={:08x}/{:08x}:{:08x}, p={:08x}/{:08x}:{:08x}, d={:08x}/{:08x}:{:08x}".format(self.count, v, i, i_exp, i_mant, p, p_exp, p_mant, d, d_exp, d_mant))
+        log("! updating predictor with: {}".format(v))
+        self.predictor.update(v)
+        self.count += 1
+        return v
+
+    def decode(self, p_exp):
+        sym = self.rc.decode_sym()
+        log("- sym=0x{:02x}".format(sym))
+        if sym == 64:
+            sign = 0
+            zeros = 32
+        else:
+            sign = sym >> 5
+            zeros = (sym & 0x1f)
+        if zeros <= 8:
+            # We need to decode some literal bits of the exponent first
+            exp_bits = 8 - zeros
+            range = 1 << exp_bits
+            expval = self.rc.decode_value(exp_bits)
+            log("- decoding exp_bits: {:02x}/{}".format(expval, exp_bits))
+            expval |= range
+        else:
+            exp_bits = 0
+            expval = 0
+
+        d_exp = expval << 23
+        if sign:
+            i_exp = (p_exp - d_exp) & 0xff800000
+        else:
+            i_exp = (p_exp + d_exp) & 0xff800000
+        dpr_exp = ((flint_untrans(i_exp) >> 23) & 0xff) - 0x7f
+        log("-- dpr_exp={}".format(dpr_exp))
+        if dpr_exp > 0x7f:
+            # Extranormal value (infinity, NaN).. don't mess with these.
+            dpr_bits = 23
+        else:
+            dpr_bits = dpr_exp - self.dpr
+            if dpr_bits < 0:
+                dpr_bits = 0
+                i = 0
+            elif dpr_bits > 23:
+                dpr_bits = 23
+        trim_bits = 23 - dpr_bits
+
+        bits = 32 - zeros - 1
+        mant_bits = max(bits - exp_bits - trim_bits, 0)
+        log("- zeros={} bits={} exp_bits={} mant_bits={} trim_bits={}".format(zeros, bits, exp_bits, mant_bits, trim_bits))
+        if mant_bits:
+            value = self.rc.decode_value(mant_bits)
+            log("- decoding mant_bits: {:x}/{}".format(value, mant_bits))
+            if not exp_bits:
+                range = 1 << mant_bits
+                value |= range
+        elif bits >= 0 and not exp_bits:
+            value = 1
+        else:
+            value = 0
+        log("- expval={:02x} valbits={:x}".format(expval, value))
+        value <<= trim_bits
+        value |= (expval << 23)
+        log("- value=0x{:x} ({})".format(value, bits))
+        return sign, value, trim_bits
 
     def done(self):
         self.rc.done()
